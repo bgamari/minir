da4bee41b6d0fd1203d28c0d82e91a598ae5d9d9
Ben Gamari <bgamari.foss@gmail.com>
2013-04-12 10:37:16 -0400

    Implement ordered index

--- SequentialDependence.hs
+++ SequentialDependence.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE TemplateHaskell, TupleSections, FlexibleInstances, BangPatterns #-}
 
 module SequentialDependence ( OrderedIndex
-                            , indexNGrams
+                            , indexTerms
+                            , nGramScore
                             ) where
 
 import           Data.HashMap.Strict (HashMap)
@@ -17,7 +18,7 @@ import Data.Foldable
 import Data.Monoid
 import Numeric.Log
 
-import TermIndex
+import TermIndex hiding (indexTerms, termScore, termDocScore)
 
 ngrams :: Int -> [a] -> [[a]]
 ngrams n = map (take n) . tails
@@ -26,26 +27,48 @@ instance Hashable a => Hashable (V.Vector a) where
     hashWithSalt salt = hashWithSalt salt . V.toList
 
 data OrderedIndex doc term
-        = OIdx { _oFreq :: HashMap (V.Vector term) (HashMap doc Int)
-                 }
+        = OIdx { _oFreq :: !(HashMap (V.Vector term) (HashMap doc Int))
+               , _oTerms :: !(HashMap (V.Vector term) Int)
+               }
         deriving (Show)
 makeLenses ''OrderedIndex
 
-instance (Hashable term, Eq term, Hashable doc, Ord doc) => Monoid (OrderedIndex doc term) where
-    mempty = OIdx HM.empty
+instance (Hashable term, Eq term, Hashable doc, Ord doc)
+         => Monoid (OrderedIndex doc term) where
+    mempty = OIdx HM.empty HM.empty
     a `mappend` b = OIdx (HM.unionWith (HM.unionWith (+)) (a^.oFreq) (b^.oFreq))
+                         (HM.unionWith (+) (a^.oTerms) (b^.oTerms))
 
-{-# INLINE indexNGrams #-}
-indexNGrams :: (Hashable doc, Hashable term, Eq term, Ord doc)
-            => Int -> doc -> [term] -> OrderedIndex doc term
-indexNGrams n doc terms = foldMap' (indexNGram doc . V.fromList) $ ngrams n terms
+{-# INLINE indexTerms #-}
+indexTerms :: (Hashable doc, Hashable term, Eq term, Ord doc)
+           => Int -> doc -> [term] -> OrderedIndex doc term
+indexTerms n doc terms = foldMap' (indexNGram doc . V.fromList) $ ngrams n terms
 
 indexNGram :: (Hashable doc, Hashable term)
            => doc -> V.Vector term -> OrderedIndex doc term
 indexNGram doc ngram = OIdx (HM.singleton ngram $ HM.singleton doc 1)
+                            (HM.singleton ngram 1)
 
 foldMap' :: (Monoid m, Foldable f) => (a -> m) -> f a -> m
 foldMap' f xs = foldl' (\a b->mappend a $ f b) mempty xs
+
+nGramScore :: (Hashable doc, Eq doc, Hashable term, Eq term)
+           => Double -> TermIndex doc term -> OrderedIndex doc term
+           -> V.Vector term -> [(doc, Score)]
+nGramScore alphaD idx oidx ngram =
+    map (\doc->(doc, nGramDocScore alphaD idx oidx ngram doc))
+    $ oidx ^. oFreq . at ngram . non HM.empty . to HM.keys
+
+nGramDocScore :: (Hashable term, Eq term, Hashable doc, Eq doc)
+              => Double -> TermIndex doc term -> OrderedIndex doc term
+              -> V.Vector term -> doc -> Score
+nGramDocScore alphaD idx oidx ngram doc =
+    (1 - ad) * realToFrac tf / d + ad * cf / c
+  where cf = oidx ^. oTerms . at ngram . non 0 . to realToFrac
+        tf = oidx ^. oFreq . at ngram . non HM.empty . at doc . non 0 . to realToFrac
+        d = idx ^. tDocs . at doc . non 0 . to realToFrac
+        c = idx ^. tTotalTerms . to realToFrac
+        ad = realToFrac alphaD
 
 
 data UnorderedStats doc term = UnorderedStats { _uFreq :: HashMap (Set term) (HashMap doc Int)
--- Test.hs
+++ Test.hs
@@ -3,9 +3,10 @@ import Data.List
 import Data.Monoid
 import qualified Data.Set as S
 import Data.Foldable
-import TermIndex
-import SequentialDependence
+import TermIndex as TI
+import SequentialDependence as SD
 import Control.Lens
+import qualified Data.Vector as V
 
 documents :: [(Int, [Int])]
 documents =
@@ -16,9 +17,11 @@ documents =
     ] ++map (\i->(i,[10..20])) [200..300000]
 
 main = do
-    let idx = foldMap' (uncurry indexTerms) documents
+    let idx = foldMap' (uncurry TI.indexTerms) documents
+        oidx = foldMap' (uncurry $ SD.indexTerms 8) documents
     print $ idx ^. tTotalTerms
     Prelude.mapM_ (print . sortBy (flip compare `on` snd) . termScore 0.1 idx) [1..5]
+    print $ sortBy (flip compare `on` snd) $ nGramScore 0.1 idx oidx $ V.fromList [1..8]
 
 foldMap' :: (Monoid m, Foldable f) => (a -> m) -> f a -> m
 foldMap' f xs = Data.Foldable.foldl' (\a b->mappend a $ f b) mempty xs
