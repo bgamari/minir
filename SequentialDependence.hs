d137adc3f44efe20f8e59e2fae829d4c38d9521c
Ben Gamari <bgamari.foss@gmail.com>
2013-04-11 20:34:11 -0400

    Rework without TermStats

--- SequentialDependence.hs
+++ SequentialDependence.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TemplateHaskell, TupleSections, TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE TemplateHaskell, TupleSections, FlexibleInstances #-}
 
 module SequentialDependence where
 
@@ -17,13 +17,8 @@ import Numeric.Log
 
 type Score = Log Double
 
--- | TermIndex is suitable for querying
-type TermIndex doc term = TermIndex' (Set (Int,doc)) doc term
--- | TermStats is suitable for building up during indexing
-type TermStats doc term = TermIndex' (HashMap doc Int) doc term
-
-data TermIndex' postings doc term
-         = TermIdx  { _tFreq :: !(HashMap term postings)
+data TermIndex doc term
+         = TermIdx  { _tFreq :: !(HashMap term (HashMap doc Int))
                       -- ^ Number of term mentions in each document
                     , _tTerms :: !(HashMap term Int)
                     , _tTotalTerms :: !Int
@@ -32,9 +27,9 @@ data TermIndex' postings doc term
                       -- ^ Number of terms in each document
                     }
          deriving (Show)
-makeLenses ''TermIndex'
+makeLenses ''TermIndex
 
-instance (Hashable term, Eq term, Hashable doc, Ord doc) => Monoid (TermStats doc term) where
+instance (Hashable term, Eq term, Hashable doc, Ord doc) => Monoid (TermIndex doc term) where
     mempty = TermIdx HM.empty HM.empty 0 HM.empty
     a `mappend` b = TermIdx (HM.unionWith (HM.unionWith (+)) (a^.tFreq) (b^.tFreq))
                             (HM.unionWith (+) (a^.tTerms) (b^.tTerms))
@@ -44,39 +39,28 @@ instance (Hashable term, Eq term, Hashable doc, Ord doc) => Monoid (TermStats do
 termScore :: (Hashable doc, Eq doc, Hashable term, Eq term)
           => Double -> TermIndex doc term -> term -> [(doc, Score)]
 termScore alphaD idx term =
-    let freqs = maybe [] S.toDescList $ idx^?tFreq.ix term
-    in map (\a@(_,doc) -> (doc, termDocScore alphaD idx term a)) freqs
+    map (\doc->(doc, termDocScore alphaD idx term doc))
+    $ idx ^. tFreq . at term . non HM.empty . to HM.keys
 
 termDocScore :: (Hashable doc, Eq doc, Hashable term, Eq term)
-             => Double -> TermIndex doc term -> term -> (Int,doc) -> Score
-termDocScore alphaD idx term (tf,doc) =
+             => Double -> TermIndex doc term -> term -> doc -> Score
+termDocScore alphaD idx term doc =
     (1 - realToFrac alphaD) * realToFrac tf / d + realToFrac alphaD * cf / c
   where cf = idx ^. tTerms . at term . non 0 . to realToFrac
+        tf = idx ^. tFreq . at term . non HM.empty . at doc . non 0 . to realToFrac
         d = idx ^. tDocs . at doc . non 0 . to realToFrac
         c = idx ^. tTotalTerms . to realToFrac
 
 indexTerms :: (Hashable doc, Hashable term, Eq term, Ord doc)
-           => doc -> [term] -> TermStats doc term
+           => doc -> [term] -> TermIndex doc term
 indexTerms doc terms = foldMap (indexTerm doc) terms
 
 indexTerm :: (Hashable doc, Hashable term)
-          => doc -> term -> TermStats doc term
+          => doc -> term -> TermIndex doc term
 indexTerm doc term = TermIdx (HM.singleton term $ HM.singleton doc 1)
                              (HM.singleton term 1)
                              1
                              (HM.singleton doc 1)
-
-termStatsToIndex :: (Ord doc) => TermStats doc term -> TermIndex doc term
-termStatsToIndex stats = stats & tFreq.mapped %~ invert
-  where invert = foldMap (\(d,n)->S.singleton (n,d)) . HM.toList
-
-termIndexToStats :: (Eq doc, Hashable doc) => TermIndex doc term -> TermStats doc term
-termIndexToStats idx = idx & tFreq.mapped %~ invert
-  where invert = foldMap (\(n,d)->HM.singleton d n) . S.toList
-
-statsIndex :: (Hashable doc, Eq doc, Ord doc)
-           => Iso' (TermStats doc term) (TermIndex doc term)
-statsIndex = iso termStatsToIndex termIndexToStats
 
 ngrams :: Int -> [a] -> [[a]]
 ngrams n = map (take n) . tails
--- Test.hs
+++ Test.hs
@@ -1,3 +1,5 @@
+import Data.Function
+import Data.List
 import qualified Data.Set as S
 import Data.Foldable
 import SequentialDependence
@@ -9,10 +11,8 @@ documents =
     , (101, [1,2,3,4])
     , (102, [1,3])
     , (103, [1,3,5])
-    ] -- ++map (\i->(i,[10..20])) [200..3000]
+    ] ++map (\i->(i,[10..20])) [200..300000]
 
 main = do
-    let stats = foldMap (uncurry indexTerms) documents
-        idx = termStatsToIndex stats
-    print $ idx ^. tFreq . at 1 . non S.empty . to S.toDescList
-    --Prelude.mapM_ (print . termScore 0.1 idx) [1..5]
+    let idx = foldMap (uncurry indexTerms) documents
+    Prelude.mapM_ (print . sortBy (flip compare `on` snd) . termScore 0.1 idx) [1..5]
