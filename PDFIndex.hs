33044bb7f8e94e88be9f72c37afc1d814939f107
Ben Gamari <bgamari.foss@gmail.com>
2013-04-13 19:29:40 -0400

    Add PDFIndex and Query

---
+++ PDFIndex.hs
@@ -0,0 +1,44 @@
+import           Control.Applicative
+import           Data.Foldable
+import           Data.Function
+import           Data.Hashable
+import           Data.List
+import           Data.Monoid
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import           System.Environment
+import           System.Exit
+import           System.IO.Error
+import           System.Process
+import           Data.Binary
+import           System.Directory
+import           Data.Text.Binary
+
+import           SequentialDependence as SD
+import           Types
+
+readPDF :: FilePath -> IO T.Text
+readPDF fname = spoon $ do
+    (inp,out,err,pid) <- runInteractiveProcess "/usr/bin/pdftotext" [fname, "-"]
+                         Nothing Nothing
+    d <- TIO.hGetContents out
+    code <- waitForProcess pid
+    case code of
+      ExitSuccess -> return d
+      otherwise   -> return T.empty
+  where spoon :: IO T.Text -> IO T.Text
+        spoon a = do b <- tryIOError a
+                     case b of
+                       Left error -> do putStrLn $ "Failed to read "++fname
+                                        return T.empty
+                       Right res  -> return res
+
+indexPDF :: FilePath -> IO (SeqDepIndex String T.Text)
+indexPDF fname = SD.fromDocument 2 fname . T.words . T.toLower <$> readPDF fname
+
+main = do
+    exists <- doesFileExist "index"
+    idx <- if exists then decodeFile "index" else return mempty
+    files <- getArgs
+    idx' <- foldlM (\a fname->mappend a <$> indexPDF fname) mempty files
+    encodeFile "index" (idx <> idx')
---
+++ Query.hs
@@ -0,0 +1,19 @@
+import qualified Data.Text as T
+import Data.Binary
+import Data.Hashable (Hashable)
+import Data.List (sortBy)
+import Data.Text.Binary
+import System.Environment (getArgs)
+import Data.Function (on)
+
+import SequentialDependence as SD
+import Types
+
+main = do
+    terms <- getArgs
+    idx <- decodeFile "index" :: IO (SeqDepIndex String T.Text)
+    mapM_ print $ take 10 $ topN idx $ map T.pack terms
+
+topN :: (Ord term, Ord doc, Hashable term, Eq term)
+     => SeqDepIndex doc term -> [term] -> [(doc, Score)]
+topN idx = sortBy (flip compare `on` snd) . scoreTerms defaultParams idx
