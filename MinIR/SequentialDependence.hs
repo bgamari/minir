353b6ed857eeee528a6b4ac294ca7a576603b8dc
Ben Gamari <bgamari.foss@gmail.com>
2013-04-18 13:30:39 -0400

    Add unordered index

--- MinIR/SequentialDependence.hs
+++ MinIR/SequentialDependence.hs
@@ -22,12 +22,13 @@ import MinIR.Types
 import qualified MinIR.CorpusStats as CS
 import qualified MinIR.TermIndex as TI
 import qualified MinIR.OrderedIndex as OI
---import qualified UnorderedIndex as UI
+import qualified MinIR.UnorderedIndex as UI
 
 data SeqDepIndex doc term
     = SDIdx { cstats :: CS.CorpusStats doc term
             , tIdx :: TI.TermIndex doc term
             , oIdx :: OI.OrderedIndex doc term
+            , uIdx :: UI.UnorderedIndex doc term
             }
     deriving (Show, Generic)
 
@@ -40,9 +41,9 @@ defaultParams = SDParams 0.1 0.1 0.1
 
 instance (Ord term, Hashable term, Ord doc, Hashable doc)
          => Monoid (SeqDepIndex doc term) where
-    mempty = SDIdx mempty mempty mempty
-    SDIdx ca ta oa `mappend` SDIdx cb tb ob =
-        SDIdx (ca <> cb) (ta <> tb) (oa <> ob)
+    mempty = SDIdx mempty mempty mempty mempty
+    SDIdx ca ta oa ua `mappend` SDIdx cb tb ob ub =
+        SDIdx (ca <> cb) (ta <> tb) (oa <> ob) (ua <> ub)
 
 fromDocument :: (Ord term, Hashable term, Ord doc, Hashable doc)
              => Int -> doc -> [term] -> SeqDepIndex doc term
@@ -50,14 +51,17 @@ fromDocument n doc terms =
     SDIdx (CS.fromDocument doc (length terms))
           (TI.fromTerms doc terms)
           (OI.fromTerms n doc terms)
+          (UI.fromTerms 8 n doc terms)
 
 scoreTerms :: (Ord term, Ord doc, Hashable term)
            => SeqDepParams -> SeqDepIndex doc term -> [term] -> [(doc, Score)]
-scoreTerms (SDParams lt lu lo) (SDIdx cstats tIdx oIdx) terms =
+scoreTerms (SDParams lt lu lo) (SDIdx cstats tIdx oIdx uIdx) terms =
     let t = TI.termsScore 0.1 cstats tIdx terms
         o = OI.termsScore 2 0.1 cstats oIdx terms
+        u = UI.termsScore 8 2 0.1 cstats uIdx terms
         docs = S.unions [M.keysSet t, M.keysSet o]
         f d = let psiT = M.findWithDefault 0 d t
                   psiO = M.findWithDefault 0 d o
-              in (d, realToFrac lt * psiT + realToFrac lo * psiO)
+                  psiU = M.findWithDefault 0 d u
+              in (d, realToFrac lt * psiT + realToFrac lo * psiO + realToFrac lo * psiU)
     in map f $ S.toList docs
--- MinIR/UnorderedIndex.hs
+++ MinIR/UnorderedIndex.hs
@@ -1,33 +1,75 @@
-{-# LANGUAGE TemplateHaskell, DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell, DeriveGeneric, RankNTypes #-}
 
-module MinIR.UnorderedIndex where
+module MinIR.UnorderedIndex ( UnorderedIndex
+                            , fromTerms
+                            , termsScore
+                            ) where
 
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
-import qualified Data.Vector as V
 
 import Data.Hashable
-import GHC.Generics
+import GHC.Generics (Generic)
 import Control.Lens
 import Data.List (tails)
-import Data.Foldable
+import Data.Foldable.Strict
 import Data.Monoid
+import Data.Binary
 import Numeric.Log
 
-import MinIR.TermIndex hiding (indexTerms, termScore, termDocScore)
+import MinIR.Types
+import MinIR.CorpusStats
 import MinIR.FreqMap (fFreqs, fTotal, FreqMap)
 import qualified MinIR.FreqMap as FM
 
-instance Hashable a => Hashable (S.Set a) where
-    hashWithSalt salt = hashWithSalt salt . S.toAscList
+ngrams :: Int -> Int -> [a] -> [[a]]
+ngrams k n =
 
 newtype UnorderedIndex doc term
         = UIdx { _oFreq :: (Map (S.Set term) (FreqMap doc)) }
         deriving (Show, Generic)
 makeLenses ''UnorderedIndex
 
+instance (Binary doc, Binary term) => Binary (UnorderedIndex doc term)
+
 instance (Ord doc, Ord term)
          => Monoid (UnorderedIndex doc term) where
     mempty = UIdx M.empty
     UIdx a `mappend` UIdx b = UIdx (M.unionWith mappend a b)
+
+{-# INLINE fromTerms #-}
+fromTerms :: (Ord term, Eq term, Ord doc)
+          => Int -> Int -> doc -> [term] -> UnorderedIndex doc term
+fromTerms k n doc terms =
+    foldMap' (fromNGram doc . S.fromList) $ ngrams k n terms
+
+{-# INLINE fromNGram #-}
+fromNGram :: (Ord doc)
+          => doc -> S.Set term -> UnorderedIndex doc term
+fromNGram doc ngram = UIdx (M.singleton ngram $ FM.singleton doc 1)
+
+def :: a -> Iso' (Maybe a) a
+def a = iso (maybe a id) Just
+
+termsScore :: (Ord doc, Ord term, Eq term)
+           => Int -> Int -> Double -> CorpusStats doc term -> UnorderedIndex doc term
+           -> [term] -> M.Map doc Score
+termsScore k n alphaD stats oidx terms =
+   let ngrams' = map S.fromList $ ngrams k n terms
+   in foldl' (M.unionWith (+)) mempty $ do
+                     ngram <- ngrams'
+                     doc <- oidx ^. oFreq . at ngram . def mempty . fFreqs . to M.keys
+                     let score = nGramDocScore alphaD stats oidx ngram doc
+                     return $ M.singleton doc score
+
+nGramDocScore :: (Ord doc, Ord term, Eq term)
+              => Double -> CorpusStats doc term -> UnorderedIndex doc term
+              -> S.Set term -> doc -> Score
+nGramDocScore alphaD stats oidx ngram doc =
+    (1 - ad) * realToFrac tf / d + ad * cf / c
+  where cf = oidx ^. oFreq . at ngram . def mempty . fTotal . to realToFrac
+        tf = oidx ^. oFreq . at ngram . def mempty . fFreqs . at doc . non 0 . to realToFrac
+        d = stats ^. cDocs . at doc . non 0 . to realToFrac
+        c = stats ^. cTotalTerms . to realToFrac
+        ad = realToFrac alphaD
