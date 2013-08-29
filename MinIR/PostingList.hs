ef8357b458adcbc1014f092da91cee02758abab9
Ben Gamari <bgamari.foss@gmail.com>
2013-08-28 21:18:50 -0400

    PostingList: Export members

--- MinIR/PostingList.hs
+++ MinIR/PostingList.hs
@@ -1,11 +1,15 @@
-module MinIR.PostingList ( -- * Reading posting lists
-                           PostingList
+module MinIR.PostingList ( -- * Postings
+                           Posting(..)
+                           -- * Reading posting lists
+                         , PostingList
                          , lookup
                          , toProducer
                            -- * Building posting lists
                          , build
+                         , PostingProducer
                          ) where
 
 import Prelude hiding (lookup)
+import MinIR.PostingList.Types
 import MinIR.PostingList.Reader
 import MinIR.PostingList.Builder
